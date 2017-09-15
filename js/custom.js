7d982a7ab43de0f834be6a800269676ae9e3f572
Adrian Pacheco <adrianpachecodesign@gmail.com>
2017-09-15 11:29:48 -0700

    Full website design update

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,31 @@
 
 $(document).ready(function() {
+     
+    
+ var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+    
+    if( isMobile.any() ) alert('This website is not optimized for mobile devices.');
+    
+}
     
     /* Every time the window is scrolled ... */
     $(window).scroll( function(){
@@ -26,6 +52,7 @@ $(document).ready(function() {
 
     
     });
+    
     
 });
 
